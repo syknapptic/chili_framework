9d742029cdd76d4514dc4633b1c8b5ffb34bd7e2
syknapptic <syknapptic@gmail.com>
2018-06-22 15:39:54 -0700

    first complete game; I did not pick the topic :|

--- Engine/Game.h
+++ Engine/Game.h
@@ -36,11 +36,56 @@ private:
 	void UpdateModel();
 	/********************************/
 	/*  User Functions              */
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+	void DrawGameOver(int x, int y);
+	void DrawTitleScreen(int x, int y);
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
+	//int ReverseX(int x, int width);
+	//int ReverseY(int y, int height);
+	bool IsColliding(int x0, int y0, int width0, int height0,
+		             int x1, int y1, int width1, int height1);
+	bool is_touching_right(int x, int width);
+	bool is_touching_left(int x, int width);
+	bool is_touching_bottom(int y, int height);
+	bool is_touching_top(int y, int height);
+	int Game::update_poo_x_direction(int x, int poo_x_direction, int poo_width);
+	int Game::update_poo_y_direction(int x, int poo_y_direction, int poo_height);
+	int update_poo_x(int x, int poo_x_direction, int poo_width);
+	int update_poo_y(int y, int poo_y_direction, int poo_height);
 	/********************************/
 private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
+	int dude_x = 400;
+	int dude_y = 300;
+	int dude_width = 20;
+	int dude_height = 20;
+
+	int poo_width = 24;
+	int poo_height = 24;
+	int poo0_x = 300;
+	int poo0_y = 150;
+	int poo0_x_direction = 1;
+	int poo0_y_direction = 1;
+	bool poo0_is_eaten = false;
+
+	int poo1_x = 100;
+	int poo1_y = 500;
+	int poo1_x_direction = 1;
+	int poo1_y_direction = 1;
+	bool poo1_is_eaten = false;
+
+	int poo2_x = 600;
+	int poo2_y = 400;
+	int poo2_x_direction = 1;
+	int poo2_y_direction = 1;
+	bool poo2_is_eaten = false;
+
+	bool is_started = false;
+	//bool is_touching_edge = false;
 	/********************************/
 };
